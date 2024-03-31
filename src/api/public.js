94d7fd21e4744c25da7e59022913849517a1a8d8
Lu Can <lucanlc0417@gmail.com>
2024-03-31 22:38:13 +0800

    报修单相关接口页面等

--- src/api/public.js
+++ src/api/public.js
@@ -2,7 +2,7 @@
 import request from "@/utils/request.js";
 // 登陆接口
 export const LOGIN = (params) => {
-  return request.get("/public/login", {params});
+  return request.get("/public/login", { params });
 };
 // 用户注册接口
 export const REGISTER = (params) => {
@@ -12,7 +12,19 @@ export const REGISTER = (params) => {
 export const GET_REPAIR_LIST = (params) => {
   return request.get("/public/repair_list");
 };
+// 根据uid获取报修信息
+export const GET_SELF_REPAIR = (uid) => {
+  return request.get("/public/repair_list/" + uid);
+};
 // 添加报修信息
 export const ADD_REPAIR = (params) => {
   return request.post("/public/add_repair", params);
+};
+// 更改报修状态
+export const UPDATE_REPAIR = (params) => {
+  return request.post("/public/change_repair_status", params);
+};
+// 填写评价
+export const ADD_APPRAISE = (params) => {
+  return request.post("/public/appraise", params);
 };
